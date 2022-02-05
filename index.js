6aa777a63cdc667f9276cc38c388d8d68d1c14be
Abhishek <abhishekc838@gmail.com>
2022-02-05 21:02:12 +0530

    ToDo list Done

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const inputTaskBox = document.querySelector('#inputBox')
 const addtaskBtn = document.querySelector('#addBtn')
 const validtext = document.querySelector("#valid-text")
@@ -7,108 +6,103 @@ const todocompleted = document.querySelector(".todo-done-list")
 
 let tasks = []
 
-onInputEnter()
+onEnter()
 
-function addtodo() {
-    let task = inputTaskBox.value
-     
-    if(!task){
-        inputTaskBox.classList.add('is-invalid')
-    }else{
-        inputTaskBox.classList.remove('is-invalid')
-        setEmptyInput()
-        tasks.push({
-            task,
-            createdOn: createDate(),
-            isDone: false
-        })
-        renderTodos() 
-    }
+function addTask() {
+  let task = inputTaskBox.value
+  if (!task) {
+    inputTaskBox.classList.add('is-invalid')
+  } else {
+    inputTaskBox.classList.remove('is-invalid')
+    tasks.push({
+      task,
+      isDone: false
+    })
+    setEmptyInput()
+    renderTodos()
+  }
 }
 
-function createDate() {
-    return moment().fromNow()
-}
-
-function onInputEnter() {
-    inputTaskBox.onkeyup = function (e) {
+function onEnter() {
+  inputTaskBox.addEventListener('keyup', (e) => {
     if (e.key === 'Enter') {
-        addtodo();
-        }
+      addTask()
     }
+  })
 }
 
 function setEmptyInput() {
-    inputTaskBox.value = ""
+  inputTaskBox.value = ""
 }
+
 
 function renderTodos() {
-    todo_list.innerHTML = ""
-    todocompleted.innerHTML = ""
+  todo_list.innerHTML = ""
+  todocompleted.innerHTML = ""
 
-    function showTask(item, index) {
-       return `<li class="item list-group-item my-1"> <div class="row">
-            <div class="col-9 d-flex align-items-center">
-            <b class="col-4 edit_text${index}">${item.task}</b>
-            <div class="input-group mb-3 d-none edit-box${index}" id="editInput">
-                <input type="text" class="form-control" id="newEditText" value="${item.task}" placeholder="Add Task">
-                <button class="btn btn-outline-secondary" type="button" onclick="saveOnEdit(${index})" id="button-addon2">Save</button>
-            </div> 
-            
-            <span class="col-4 text-center"> ${item.createdOn} </span>
-            </div>
-            <div class="col-1"><button type="button" onclick="editodo(${index})" class="btn complete-item item-icon"><i class="bi bi-pencil-square"></i></button> </div>
-            <div class="col-1"><button type="button" onclick="completedTodo(${index})" class="btn complete-item item-icon"><i class="bi bi-check2-circle"></i></button> </div>
-            <div class="col-1"><button type="button" onclick="deleteTodo(${index})" class="btn delete-item text-danger item-icon"><i class="bi bi-x-circle"></i></button></div></div>
-        </li>`
+  function taskHtml(element, index) {
+    const {task} = element || {}
+    return `<li class="item list-group-item my-1"> <div class="row">
+
+              <div class="col-8 d-flex align-items-center">
+                <b class="col-4 edit_text${index}">${task}</b>
+                <div class="input-group mb-3 d-none edit-box${index}" id="editInput">
+                    <input type="text" class="form-control" id="newEditText" value="${task}" placeholder="Add Task">
+                    <button class="btn btn-outline-secondary" type="button" onclick="saveOnEdit(${index})" id="button-addon2">Save</button>
+                </div> 
+                
+              </div>
+
+              <div class="col-1"><button type="button" onclick="editodo(${index})" class="btn item-icon"><i class="bi bi-pencil-square"></i></button> </div>
+              <div class="col-1 complete-item"><button type="button" onclick="completedTodo(${index})" class="btn complete-item item-icon"><i class="bi bi-check2-circle"></i></button> </div>
+              
+              <div class="col-1"><button type="button" onclick="deleteTodo(${index})" class="btn delete-item text-danger item-icon"><i class="bi bi-x-circle"></i></button></div></div>
+          </li>`
+  }
+
+  let taskitem = "";
+  let completeitem = "";
+
+  tasks.forEach((element, index) => {
+    const {task, isDone} = element || {};
+
+    let htmlCode = taskHtml(element, index)
+
+    if (isDone) {
+      completeitem += htmlCode
+    } else {
+      taskitem += htmlCode
     }
+  })
 
-    let tasksHtmlList = "";
-    let completTaskHtmlList = "";
-    
-    tasks.forEach((item, index) => {
-        const {isDone} = item || {}
-        let htmlCode = showTask(item, index)
-
-        if(isDone){
-            completTaskHtmlList += htmlCode
-        }else{
-            tasksHtmlList += htmlCode
-        }
-    });
-
-    todo_list.innerHTML = tasksHtmlList
-    todocompleted.innerHTML = completTaskHtmlList
+  todo_list.innerHTML = taskitem
+  todocompleted.innerHTML = completeitem
 }
 
-function deleteTodo(itemIndex){
-    tasks = tasks.filter((_, index) => index !== itemIndex)
-    renderTodos()
+function deleteTodo(itemIndex) {
+  
+  tasks = tasks.filter((_, index) => index !== itemIndex)
+  renderTodos()
 }
 
 function completedTodo(itemIndex) {
-    const task = tasks[itemIndex] 
-    task.isDone = true 
-    tasks[itemIndex] = task
-
-    renderTodos()
+  const task = tasks[itemIndex]
+  task.isDone = !task.isDone
+  tasks[itemIndex] = task
+  renderTodos()
 }
-
 
 function editodo(itemIndex) {
-    const task = tasks[itemIndex]
-    const editTextBox = document.querySelector(`.edit_text${itemIndex}`)
-    editTextBox.classList.add('d-none')
-    const editInputBox = document.querySelector(`.edit-box${itemIndex}`)
-    editInputBox.classList.remove('d-none')   
+  const editTextDiv = document.querySelector(`.edit_text${itemIndex}`)
+  editTextDiv.classList.add('d-none')
+  const editInputBox = document.querySelector(`.edit-box${itemIndex}`)
+  editInputBox.classList.remove('d-none')
 }
 
 
-function saveOnEdit(itemIndex){
-    
-    const editInputBox = document.querySelector(`.edit-box${itemIndex} input`)
-    const updatedTask = editInputBox.value
-    tasks[itemIndex].task = updatedTask
-    renderTodos()   
-    
+function saveOnEdit(itemIndex) {
+  const editInputBox = document.querySelector(`.edit-box${itemIndex} input`)
+  const updatedTask = editInputBox.value
+  tasks[itemIndex].task = updatedTask
+  renderTodos()
 }
